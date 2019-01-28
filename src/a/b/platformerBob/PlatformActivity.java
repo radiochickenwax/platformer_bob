22232959f08c04b1bcb17486df83e146b04b4cb3
Greg Ubuntu Chroot ZX551ml <gregorio.jp.mitchell@gmail.com>
2019-01-27 22:27:59 -0700

    empty ant project

---
+++ src/a/b/platformerBob/PlatformActivity.java
@@ -0,0 +1,15 @@
+package a.b.platformerBob;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlatformActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
