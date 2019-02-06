8d9ba988670e2f488d080668c7b1c0d5c368e06f
Greg Ubuntu Chroot ZX551ml <gregorio.jp.mitchell@gmail.com>
2019-02-05 20:10:19 -0700

    build error: gearing up to add PlatformView.java

--- src/a/b/platformerBob/PlatformActivity.java
+++ src/a/b/platformerBob/PlatformActivity.java
@@ -2,14 +2,52 @@ package a.b.platformerBob;
 
 import android.app.Activity;
 import android.os.Bundle;
+import   android.graphics.Point;
+import   android.os.Bundle;
+import   android.view.Display;
 
 public class PlatformActivity extends Activity
 {
+    // Our object to handle the View
+    private PlatformView platformView;
+
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	// Get a Display object to access screen details
+	Display display = getWindowManager().getDefaultDisplay();
+
+	// Load the resolution into a Point object
+	Point resolution = new Point();
+	display.getSize(resolution);
+
+	// And finally set the view for our game
+	// Also passing in the screen resolution
+	platformView = new PlatformView
+	    (this, resolution.x, resolution.y);
+
+	// Make our platformView the view for the Activity
+	setContentView(platformView);
+
+
+	//setContentView(R.layout.main);
     }
+
+    // If the Activity is paused make sure to pause our thread
+    @Override
+    protected void onPause() {
+	super.onPause();
+	platformView.pause();
+    }
+
+    // If the Activity is resumed make sure to resume our thread
+    @Override
+    protected void onResume() {
+	super.onResume();
+	platformView.resume();
+    }
+
 }
