c584475281f5716cbbc85ec6a32af98c9eff40d7
Greg Ubuntu Chroot ZX551ml <gregorio.jp.mitchell@gmail.com>
2019-02-07 21:55:49 -0700

    rebuild

--- bin/classes.dex.d
+++ bin/classes.dex.d
@@ -1,6 +1,7 @@
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes.dex : \
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/R$layout.class \
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/Grass.class \
+/cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/Viewport.class \
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/R$string.class \
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/BuildConfig.class \
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/PlatformActivity.class \
@@ -10,4 +11,5 @@
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/R.class \
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/R$drawable.class \
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/PlatformView.class \
+/cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/Player.class \
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/Vector2Point5D.class \
