5ea9e982531c48f3ba2669c6c77ad6105ffbb9d5
Greg Ubuntu Chroot ZX551ml <gregorio.jp.mitchell@gmail.com>
2019-02-07 21:42:57 -0700

    rebuild

--- bin/classes.dex.d
+++ bin/classes.dex.d
@@ -1,10 +1,12 @@
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes.dex : \
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/R$layout.class \
+/cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/Grass.class \
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/R$string.class \
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/BuildConfig.class \
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/PlatformActivity.class \
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/R$attr.class \
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/LevelManager.class \
+/cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/GameObject.class \
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/R.class \
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/R$drawable.class \
 /cf/home/notes/android/sketches/2019-01-27/PlatformerBob/bin/classes/a/b/platformerBob/PlatformView.class \
