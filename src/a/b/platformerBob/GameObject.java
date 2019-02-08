9e85e93e5ae3285fd91ad841431e58930406616f
Greg Ubuntu Chroot ZX551ml <gregorio.jp.mitchell@gmail.com>
2019-02-07 21:39:31 -0700

    fleshed out GameObject.java

--- src/a/b/platformerBob/GameObject.java
+++ src/a/b/platformerBob/GameObject.java
@@ -17,5 +17,87 @@ public abstract class GameObject{
 
     private String bitmapName;
 
+    public String getBitmapName() {
+	return bitmapName;
+    }
+
+    public Bitmap prepareBitmap(Context context,
+				String bitmapName,
+				int pixelsPerMetre) {
+
+
+        // Make a resource id from the bitmapName
+        int resID = context.getResources().
+	    getIdentifier(bitmapName,
+			  "drawable", context.getPackageName());
+
+
+	// Create the bitmap
+	Bitmap bitmap = BitmapFactory.
+	    decodeResource(context.getResources(),
+			   resID);
+
+	// Scale the bitmap based on the number of pixels per metre
+	// Multiply by the number of frames in the image
+	// Default 1 frame
+	bitmap = Bitmap.createScaledBitmap(bitmap,
+					   (int) (width * animFrameCount * pixelsPerMetre),
+					   (int) (height * pixelsPerMetre),
+					   false);
+
+	return bitmap;
+    }
+
+    public Vector2Point5D getWorldLocation() {
+	return worldLocation;
+    }
+
+    public void setWorldLocation(float x, float y, int z) {
+	this.worldLocation = new Vector2Point5D();
+	this.worldLocation.x = x;
+	this.worldLocation.y = y;
+	this.worldLocation.z = z;
+    }
+
+    public void setBitmapName(String bitmapName){
+	this.bitmapName = bitmapName;
+    }
+
+    public float getWidth() {
+	return width;
+    }
+
+    public void setWidth(float width) {
+	this.width = width;
+    }
+
+    public float getHeight() {
+	return height;
+    }
+
+    public void setHeight(float height) {
+	this.height = height;
+    }
+
+
+    public boolean isActive() {
+	return active;
+    }
+
+    public boolean isVisible() {
+	return visible;
+    }
+
+    public void setVisible(boolean visible) {
+	this.visible = visible;
+    }
+
+    public char getType() {
+	return type;
+    }
+
+    public void setType(char type) {
+	this.type = type;
+    }
 
 }
